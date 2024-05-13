4e70f74b189579aec9d8cb0226d4a9ffbdc67039
Izzy Lancaster <avrisaac555@gmail.com>
2024-05-12 18:17:46 -0700

    Add a new unit test for padding

--- Tests/Tests.Fundament.Capstone.Runtime/MessageStreamReaderTests.cs
+++ Tests/Tests.Fundament.Capstone.Runtime/MessageStreamReaderTests.cs
@@ -5,6 +5,9 @@ using System.Collections.Immutable;
 using FluentAssertions.Execution;
 
 using global::Fundament.Capstone.Runtime.BinaryStream;
+
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
 using Xunit.Abstractions;
 
 public class MessageStreamReaderTests(ITestOutputHelper outputHelper)
@@ -18,6 +21,7 @@ public class MessageStreamReaderTests(ITestOutputHelper outputHelper)
             0x3F, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
         ];
 
+    public const ulong EmptyStructPointer = 0x000000000000C03F;
 
     [Fact]
     public async Task ReadSimpleMessage()
@@ -30,7 +34,32 @@ public class MessageStreamReaderTests(ITestOutputHelper outputHelper)
         using (new AssertionScope()) {
             message.SegmentCount.Should().Be(1);
             message[0].Length.Should().Be(1);
-            message[0].Span[0].Should().Be(0x000000000000C03F);
+            message[0].Span[0].Should().Be(EmptyStructPointer);
+        }
+    }
+
+    [Fact]
+    public async Task ReadHandlesPadding()
+    {
+        byte[] bytes = [
+            0x01, 0x00, 0x00, 0x00,
+            0x01, 0x00, 0x00, 0x00,
+            0x01, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,
+            0x3F, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x3F, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+        using var stream = new MemoryStream(bytes);
+        var sut = new StreamMessageReader(stream, outputHelper.ToLogger<StreamMessageReader>());
+
+        using var message = await sut.ReadMessageAsync(); 
+
+        using (new AssertionScope()) {
+            message.SegmentCount.Should().Be(2);
+            message[0].Length.Should().Be(1);
+            message[1].Length.Should().Be(1);
+            message[0].Span[0].Should().Be(EmptyStructPointer);
+            message[1].Span[0].Should().Be(EmptyStructPointer);
         }
     }
 }
