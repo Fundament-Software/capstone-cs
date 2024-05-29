e3e8543557da64ad3857d0ba4af079a21f41965d
Izzy Lancaster <avrisaac555@gmail.com>
2024-05-29 14:52:01 -0700

    Fix BitMaskOf

--- Fundament.Capstone.Runtime/src/Useful/Bits.cs
+++ Fundament.Capstone.Runtime/src/Useful/Bits.cs
@@ -2,5 +2,8 @@ namespace Fundament.Capstone.Runtime;
 
 internal static class Bits
 {
-    public static ulong BitMaskOf(int bits) => (1UL << bits) - 1;
+    public static ulong BitMaskOf(int bits) => 
+        bits == (sizeof(ulong) * 8) 
+            ? ulong.MaxValue
+            : (1UL << bits) - 1;
 }
