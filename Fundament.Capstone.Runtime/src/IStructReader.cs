cfca5c737ff71c64417fdd22d1b3bc0bce7642ed
Izzy Lancaster <avrisaac555@gmail.com>
2024-05-29 14:51:24 -0700

    Add ReadData to IStructReader

--- Fundament.Capstone.Runtime/src/IStructReader.cs
+++ Fundament.Capstone.Runtime/src/IStructReader.cs
@@ -1,5 +1,7 @@
 namespace Fundament.Capstone.Runtime;
 
+using System.Numerics;
+
 /// <summary>
 /// Interface for defining an object that can read values from a struct in a cap'n proto message.
 /// </summary>
@@ -19,10 +21,29 @@ public interface IStructReader
     public void ReadVoid(int offset);
 
     /// <summary>
+    /// Reads a value of type <typeparamref name="T"/> from the struct at the given type-aligned offset from the start of the struct's data section.
+    /// </summary>
+    /// <typeparam name="T">The type of the data being read.</typeparam>
+    /// <param name="offset">The offset of the data being read, in number of <typeparamref name="T"/>s, from the beginning of the data section.</param>
+    /// <param name="defaultValue">The default value of the data.</param>
+    /// <returns>The value of type <typeparamref name="T"/> at the provided offset.</returns>   
+    /// <remarks>
+    /// Old versions of a schema may specify structs with fields that are not present in the current version of the schema.
+    /// For this reason, Read never throw an exception if the offset is out of range. Instead, the default value should be returned.
+    /// <remarks>
+    public T ReadData<T>(int offset, T defaultValue) where T : unmanaged, IBinaryNumber<T>;
+
+    // public object ReadPointer(int offset);
+
+    /// <summary>
     /// Reads a boolean value from the struct at the given offset from the start of the struct's data section.
     /// </summary>
     /// <param name="offset">The offset in bits.</param>
     /// <param name="defaultValue">The default value of the bool field.</param>
+    /// <remarks>
+    /// Unlike other Read* definitions in the interface, this method cannot have a default implementation because in Cap'n Proto, 
+    /// boolean values are stored as bits, whereas C# stores them as bytes.
+    /// <remarks>
     /// <returns></returns>
     public bool ReadBool(int offset, bool defaultValue);
 
@@ -32,7 +53,7 @@ public interface IStructReader
     /// <param name="offset">The offset in bytes.</param>
     /// <param name="defaultValue">The default value of the Int8 field.</param>
     /// <returns></returns>
-    public sbyte ReadInt8(int offset, sbyte defaultValue);
+    public sbyte ReadInt8(int offset, sbyte defaultValue) => this.ReadData(offset, defaultValue);
 
     /// <summary>
     /// Reads a 16-bit signed integer from the struct at the given offset from the start of the struct's data section.
@@ -40,23 +61,21 @@ public interface IStructReader
     /// <param name="offset">The offset in 4-bytes</param>
     /// <param name="defaultValue">The default value of the Int16 field.</param>
     /// <returns></returns>
-    public short ReadInt16(int offset, short defaultValue);
+    public short ReadInt16(int offset, short defaultValue) => this.ReadData(offset, defaultValue);
 
-    public int ReadInt32(int offset, int defaultValue);
+    public int ReadInt32(int offset, int defaultValue) => this.ReadData(offset, defaultValue);
 
-    public long ReadInt64(int offset, long defaultValue);
+    public long ReadInt64(int offset, long defaultValue) => this.ReadData(offset, defaultValue);
 
-    public byte ReadUInt8(int offset, byte defaultValue);
+    public byte ReadUInt8(int offset, byte defaultValue) => this.ReadData(offset, defaultValue);
 
-    public ushort ReadUInt16(int offset, ushort defaultValue);
+    public ushort ReadUInt16(int offset, ushort defaultValue) => this.ReadData(offset, defaultValue);
 
-    public uint ReadUInt32(int offset, uint defaultValue);
+    public uint ReadUInt32(int offset, uint defaultValue) => this.ReadData(offset, defaultValue);
 
-    public ulong ReadUInt64(int offset, ulong defaultValue);
+    public ulong ReadUInt64(int offset, ulong defaultValue) => this.ReadData(offset, defaultValue);
 
-    public float ReadFloat32(int offset, float defaultValue);
+    public float ReadFloat32(int offset, float defaultValue) => this.ReadData(offset, defaultValue);
 
-    public double ReadFloat64(int offset, double defaultValue);
-
-    // public object ReadPointer(int offset);
+    public double ReadFloat64(int offset, double defaultValue) => this.ReadData(offset, defaultValue);
 }
--- Fundament.Capstone.Runtime/src/MessageStream/StructReader.cs
+++ Fundament.Capstone.Runtime/src/MessageStream/StructReader.cs
@@ -21,44 +21,8 @@ public sealed class StructReader(WireSegmentSlice dataSection, WireSegmentSlice 
         // All ReadVoid should do is increment the traversal limit counter by one word.
     }
 
+    public T ReadData<T>(int offset, T defaultValue) where T : unmanaged, IBinaryNumber<T> => 
+        this.dataSection.GetBySizeAlignedOffset<T>(offset) ^ defaultValue;
+
     public bool ReadBool(int offset, bool defaultValue) => this.dataSection.GetBitByOffset(offset) ^ defaultValue;
-
-    public sbyte ReadInt8(int offset, sbyte defaultValue) => this.ReadT(offset, defaultValue);
-
-    public short ReadInt16(int offset, short defaultValue) => this.ReadT(offset, defaultValue);
-
-    public int ReadInt32(int offset, int defaultValue) => this.ReadT(offset, defaultValue);
-
-    // Micro-optimization: GetBySizeAlignedOffset will work with words, but may have more branches than just checking the offset.
-    public long ReadInt64(int offset, long defaultValue) => 
-        this.dataSection.IsIndexInRange(offset)
-            ? (long) this.dataSection[offset]
-            : defaultValue;
-     
-    public byte ReadUInt8(int offset, byte defaultValue) => this.ReadT(offset, defaultValue);
-
-    public ushort ReadUInt16(int offset, ushort defaultValue) => this.ReadT(offset, defaultValue);
-
-    public uint ReadUInt32(int offset, uint defaultValue) => this.ReadT(offset, defaultValue);
-
-    // Same micro-optimization as ReadInt64
-    public ulong ReadUInt64(int offset, ulong defaultValue) => 
-        this.dataSection.IsIndexInRange(offset)
-            ? this.dataSection[offset]
-            : defaultValue;
-
-    public float ReadFloat32(int offset, float defaultValue) => this.ReadT(offset, defaultValue);
-
-    public double ReadFloat64(int offset, double defaultValue) => 
-        this.dataSection.IsIndexInRange(offset)
-            ? BitConverter.UInt64BitsToDouble(this.dataSection[offset] ^ BitConverter.DoubleToUInt64Bits(defaultValue))
-            : defaultValue;
-
-    // Generic implementation of the Read* methods.
-    // May want to promote this to the interface.
-    // Most of the reason for this method's existence is to be able to use .NET 7's generic math,
-    // which defines the bitwise xor operators for all numeric types, including those which normally 
-    // don't have bitwise operators, like float and double.
-    private T ReadT<T>(int offset, T defaultValue) where T : struct, IBinaryNumber<T> => 
-        T.CreateChecked(this.dataSection.GetBySizeAlignedOffset<T>(offset) ^ defaultValue);
 }
