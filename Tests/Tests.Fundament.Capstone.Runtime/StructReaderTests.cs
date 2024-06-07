06c8fde6cf6161475f09c7aecd068b0599b61ea2
Izzy Lancaster <avrisaac555@gmail.com>
2024-06-07 15:31:28 -0700

    Added test for reading a struct pointer

--- Tests/Tests.Fundament.Capstone.Runtime/StructReaderTests.cs
+++ Tests/Tests.Fundament.Capstone.Runtime/StructReaderTests.cs
@@ -1,5 +1,7 @@
 namespace Tests.Fundament.Capstone.Runtime;
 
+using CommunityToolkit.Diagnostics;
+
 using global::Fundament.Capstone.Runtime;
 using global::Fundament.Capstone.Runtime.MessageStream;
 
@@ -8,7 +10,7 @@ using Xunit.Abstractions;
 public class StructReaderTests(ITestOutputHelper outputHelper)
 {
     [Fact]
-    public void StructReaderReadsSimpleValue()
+    public void ReadsShort()
     {
         // 15 encoded as the first 16-bit integer in the data section.
         var (pointer, message) = CreateStructMessage([0x0000_0000_0000_000F]);
@@ -18,7 +20,7 @@ public class StructReaderTests(ITestOutputHelper outputHelper)
     }
 
     [Fact]
-    public void StructReaderReadsFloat()
+    public void ReadsFloat()
     {
         var expected = 8.62f;
         var (pointer, message) = CreateStructMessage([BitConverter.SingleToUInt32Bits(expected)]);
@@ -35,6 +37,35 @@ public class StructReaderTests(ITestOutputHelper outputHelper)
         var (structReader, _) = this.CreateStructReader(pointer, message);
 
         (structReader as IStructReader<object>).ReadFloat64(0, 0).Should().Be(expected);
+    }
+
+    [Fact]
+    public void ReadsStructPointer()
+    {
+        var expectedRootData = 927.32;
+        ulong expectedSecondaryData = 45;
+
+        var segment = new ulong[4];
+
+        // Create the root struct pointer and set the root data.
+        var rootStructPointer = new StructPointer(0, 0, 1, 1);
+        segment[0] = rootStructPointer.AsWord;
+        segment[1] = BitConverter.DoubleToUInt64Bits(expectedRootData);
+
+        // Create the secondary struct pointer and set the secondary data.
+        var secondaryStructPointer = new StructPointer(2, 0, 1, 0);
+        segment[2] = secondaryStructPointer.AsWord;
+        segment[3] = expectedSecondaryData;
+
+        var message = new WireMessage([segment]);
+        var (rootStructReader, _) = this.CreateStructReader(rootStructPointer, message);
+
+        // Assertions
+        (rootStructReader as IStructReader<object>).ReadFloat64(0, 0).Should().Be(expectedRootData);
+        var secondaryStructReader = rootStructReader.ReadPointer(0);
+        secondaryStructReader.Should().NotBeNull();
+        secondaryStructReader.Value.Should().BeOfType<StructReader<object>>();
+        (secondaryStructReader.AsT0 as IStructReader<object>).ReadUInt64(0, 0).Should().Be(expectedSecondaryData);
     }
 
     private static (StructPointer Pointer, WireMessage Message) CreateStructMessage(ulong[]? data = null, ulong[]? pointers = null)
