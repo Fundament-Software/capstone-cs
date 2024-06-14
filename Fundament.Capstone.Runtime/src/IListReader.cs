2b0bc8dbbc30cf4d51d11452716640f92350e275
Izzy Lancaster <avrisaac555@gmail.com>
2024-06-13 19:48:29 -0700

    Define IListReader

--- Fundament.Capstone.Runtime/src/IListReader.cs
+++ Fundament.Capstone.Runtime/src/IListReader.cs
@@ -1,5 +1,9 @@
 namespace Fundament.Capstone.Runtime;
 
-public interface IListReader
+/// <summary>
+/// Defines an object that can read values from a list in a Cap'n Proto message.
+/// For primitive types, this will be a list of values. For composite types or pointers, this will be a list of readers.
+/// </summary>
+public interface IListReader<TCap, TReader> : IReadOnlyList<TReader>
 {
 }
