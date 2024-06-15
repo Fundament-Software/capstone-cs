5a2faa1cb50b79b8d8b844f12bc842463482dd5a
Izzy Lancaster <avrisaac555@gmail.com>
2024-06-15 16:34:58 -0700

    Added ListPrimitiveReader

---
+++ Fundament.Capstone.Runtime/src/MessageStream/Reader/ListPrimitiveReader.cs
@@ -0,0 +1,41 @@
+namespace Fundament.Capstone.Runtime;
+
+using System.Numerics;
+
+using CommunityToolkit.Diagnostics;
+
+using Fundament.Capstone.Runtime.MessageStream;
+
+/// <summary>
+/// A reader for a list of primitive values. This shoud NOT be used for bools. Use <see cref="BoolListReader"/> instead.
+/// </summary>
+/// <typeparam name="T">The type of the primitive values in the list. This should not be `bool`.</typeparam>
+/// <typeparam name="TCap">The type of the capability table imbued in the reader.</typeparam>
+public class ListPrimitiveReader<T, TCap> : AbstractBaseListReader<T, TCap, ListPrimitiveReader<T, TCap>>
+where T : unmanaged, IBinaryNumber<T>
+{
+    internal ListPrimitiveReader(SharedReaderState state, int segmentId, Index pointerIndex, ListPointer pointer)
+        : base(state, segmentId, pointerIndex, pointer)
+    {
+    }
+
+    public override T this[int index]
+    {
+        get
+        {
+            Guard.IsInRangeFor(index, this);
+            return this.ListSlice.GetBySizeAlignedOffset<T>(index);
+        }
+    }
+
+    private protected override int CalculateCount(ListPointer pointer) => (int)pointer.Size;
+}
+
+public class ListPrimitiveReader<T> : ListPrimitiveReader<T, Unit>
+where T : unmanaged, IBinaryNumber<T>
+{
+    internal ListPrimitiveReader(SharedReaderState state, int segmentId, Index pointerIndex, ListPointer pointer)
+        : base(state, segmentId, pointerIndex, pointer)
+    {
+    }
+}
