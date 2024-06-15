cff9e23b7590a61ad6bca868b3a4c3086e6614b4
Izzy Lancaster <avrisaac555@gmail.com>
2024-06-15 16:32:59 -0700

    Added unit type

---
+++ Fundament.Capstone.Runtime/src/Unit.cs
@@ -0,0 +1,29 @@
+﻿namespace Fundament.Capstone.Runtime;
+
+/// <summary>
+/// A utility "unit" type that represents a valueless type.
+/// </summary>
+/// <remarks>
+/// This type is useful for representing a valueless type in a type-safe manner.
+/// C# has the `void` keyword, but it's not a type and can't be used as a type argument.
+/// </remarks>
+public class Unit : IComparable
+{
+    private Unit()
+    {
+    }
+
+    public static Unit Value { get; } = new Unit();
+
+    public override int GetHashCode() => 0;
+
+    public override bool Equals(object? obj) => obj switch {
+        Unit _ => true,
+        null => true,
+        _ => false,
+    };
+
+    public override string ToString() => "Unit";
+
+    public int CompareTo(object? obj) => 0;
+}
