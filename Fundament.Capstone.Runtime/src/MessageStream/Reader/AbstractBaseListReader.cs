59a2a90a52bf8991246e5660d3d8fc0bdb04df17
Izzy Lancaster <avrisaac555@gmail.com>
2024-06-15 15:16:28 -0700

    refactor: Move target range calculation to it's own method

--- Fundament.Capstone.Runtime/src/MessageStream/Reader/AbstractBaseListReader.cs
+++ Fundament.Capstone.Runtime/src/MessageStream/Reader/AbstractBaseListReader.cs
@@ -2,6 +2,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 using CommunityToolkit.Diagnostics;
 
@@ -39,6 +40,23 @@ where TSelf : AbstractBaseListReader<T, TCap, TSelf>
     IEnumerator IEnumerable.GetEnumerator() => new ListReaderEnumerator(this);
 
     /// <summary>
+    /// Calculates the range of the wire message that the ListPointer points to.
+    /// Or in other words, the indecies of the wire message where the list data resides.
+    /// This is used by <see cref="EvaluatePointerTarget(int, Index, ListPointer)"/> to get the slice of the wire message.
+    /// </summary>
+    /// <param name="pointerIndex">The index of the pointer in the segment.</param>
+    /// <param name="pointer">The pointer to evaluate.</param>
+    /// <returns>A Range locating the list data in the WireMessage.</returns>
+    [Pure]
+    private protected virtual Range GetPointerTargetRange(Index pointerIndex, ListPointer pointer)
+    {
+        var startIndex = pointerIndex.AddOffset(pointer.Offset + 1);
+        var endIndex = startIndex.AddOffset((int)pointer.SizeInWords);
+
+        return startIndex..endIndex;
+    }
+
+    /// <summary>
     /// Evaluates the target of a pointer and returns the slice of the wire message that the pointer points to.
     /// </summary>
     /// <param name="segmentId">The segment id of the pointer.</param>
@@ -46,14 +64,12 @@ where TSelf : AbstractBaseListReader<T, TCap, TSelf>
     /// <param name="pointer">The pointer to evaluate.</param>
     /// <returns>The slice of the wire message that the pointer points to.</returns>
     /// <exception cref="PointerOffsetOutOfRangeException">Thrown when the pointer points outside the bounds of the segment.</exception>
-    private protected virtual WireSegmentSlice EvaluatePointerTarget(int segmentId, Index pointerIndex, ListPointer pointer)
+    private WireSegmentSlice EvaluatePointerTarget(int segmentId, Index pointerIndex, ListPointer pointer)
     {
-        var startIndex = pointerIndex.AddOffset(pointer.Offset);
-        var endIndex = startIndex.AddOffset((int)pointer.SizeInWords);
-
         try
         {
-            return this.SharedReaderState.WireMessage.Slice(segmentId, startIndex..endIndex);
+            var targetRange = this.GetPointerTargetRange(pointerIndex, pointer);
+            return this.SharedReaderState.WireMessage.Slice(segmentId, targetRange);
         }
         catch (IndexOutOfRangeException e)
         {
