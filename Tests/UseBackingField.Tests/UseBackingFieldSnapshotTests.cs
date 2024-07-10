2d3175a193cab1987c96dfde2b4c439c122e2b35
Izzy Lancaster <avrisaac555@gmail.com>
2024-07-10 14:52:40 -0700

    Write simple test case

---
+++ Tests/UseBackingField.Tests/UseBackingFieldSnapshotTests.cs
@@ -0,0 +1,50 @@
+﻿namespace UseBackingField.Tests;
+
+using System.Runtime.CompilerServices;
+
+using Microsoft.CodeAnalysis.CSharp;
+
+public class UseBackingFieldSnapshotTests
+{
+    [ModuleInitializer]
+    public static void Init() =>
+        VerifySourceGenerators.Initialize();
+
+    [Fact]
+    public async Task TestSimpleClass()
+    {
+        var source = """
+        namespace Tests;
+
+        using UseBackingField;
+
+        [GenerateBackingFields]
+        public class Test
+        {
+            public string Property
+            {
+                get => this.__property;
+                set => this.__property = value;
+            }
+        }
+        """;
+
+        var driver = GeneratorDriver(source);
+        await Verify(driver);
+    }
+
+    public static CSharpGeneratorDriver GeneratorDriver(string source)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "Tests",
+            syntaxTrees: [syntaxTree]
+        );
+
+        var generator = new BackingFieldsGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGenerators(compilation);
+
+        return driver;
+    }
+}
