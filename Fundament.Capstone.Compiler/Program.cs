ad9c17502610538c2927ad23996685867b4f61d4
Izzy Lancaster <avrisaac555@gmail.com>
2024-07-18 18:31:07 -0700

    Setup first steps for code gen

--- Fundament.Capstone.Compiler/Program.cs
+++ Fundament.Capstone.Compiler/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Xml.Serialization;
+
+using Capnp;
+
+if (!Console.IsInputRedirected)
+{
+    Console.WriteLine($"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}");
+    Console.WriteLine("Expecting binary-ecoded code generation from stdin...");
+}
+
+var input = Console.OpenStandardInput();
+if (input == null)
+{
+    Console.Error.WriteLine("Failed to open stdin.");
+    return 1;
+}
+
+var segments = Framing.ReadSegments(input);
+var root = DeserializerState.CreateRoot(segments);
+var reader = Capnp.Schema.CodeGeneratorRequest.READER.create(root);
+
+return 0;
