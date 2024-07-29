bc16d1acf8f211b6e104406e119a06a8a01b3c67
Izzy Lancaster <avrisaac555@gmail.com>
2024-07-28 17:18:05 -0700

    refactoring the code

--- Fundament.Capstone.Compiler/Program.cs
+++ Fundament.Capstone.Compiler/Program.cs
@@ -1,56 +1,68 @@
-﻿using System.Reflection;
+﻿namespace Fundament.Capstone.Compiler;
+
+using System.Reflection;
 
 using Capnp;
 using Capnp.Schema;
 
-var versionLine = $"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}";
+public static class Program 
+{
+    public static readonly string VersionLine = $"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}";
 
-// Parse command-line arguments
-Stream input;
-
-if (args is [ "--version" ])
-{
-    Console.WriteLine(versionLine);
-    return 0;
-}
-else if (args is [ "--help"])
-{
-    Console.WriteLine(versionLine);
-    Console.WriteLine("Normally invoked by the Capstone compiler, this program reads a binary-encoded code generation request from a file or stdin if no file is provided.");
-    Console.WriteLine("Usage: capstone-csharp-compiler [path]");
-    Console.WriteLine("  --version  Print version information");
-    Console.WriteLine("  --help     Print this help");
-    Console.WriteLine("  [path]     Read binary-encoded code generation from file at [path]");
-    return 0;
-}
-else if (args is [ var pathArg ])
-{
-    input = File.OpenRead(pathArg);
-}
-else
-{
-    if (!Console.IsInputRedirected)
+    public static int Main(string[] args)
     {
-        Console.WriteLine(versionLine);
-        Console.WriteLine("Expecting binary-ecoded code generation from stdin...");
+        var inputStream = ParseArgs(args);
+        if (inputStream is null) {
+            return 0;
+        }
+
+        var segments = Framing.ReadSegments(inputStream);
+        var root = DeserializerState.CreateRoot(segments);
+        var reader = CodeGeneratorRequest.READER.create(root);
+
+        foreach (var node in reader.Nodes) {
+            Console.WriteLine($"Node: {node.DisplayName}");
+        }
+
+        return 0;
     }
 
-    input = Console.OpenStandardInput();
+    private static Stream? ParseArgs(string[] args) => args switch
+    {
+        { Length: >= 2 } => throw new ArgumentException("Too many arguments"),
+        ["--version"] => PrintVersion(),
+        ["--help"] => PrintHelp(),
+        [ var filePath ] => File.OpenRead(filePath),
+        _ => OpenStdin(),
+    };
+
+    private static Stream? PrintVersion()
+    {
+        Console.WriteLine(VersionLine);
+        return null;
+    }
+
+    private static Stream? PrintHelp()
+    {
+        Console.WriteLine($"""  
+            {VersionLine}
+            Normally invoked by the Capstone compiler, this program reads a binary-encoded code generation request from a file or stdin if no file is provided.
+            Usage: capstone-csharp-compiler [path]
+            --version  Print version information
+            --help     Print this help");
+            [path]     Read binary-encoded code generation from file at [path]
+        """);
+        return null;
+    }
+
+    private static Stream OpenStdin()
+    {
+        if (!Console.IsInputRedirected)
+        {
+            Console.WriteLine(VersionLine);
+            Console.WriteLine("Expecting binary-encoded code generation from stdin...");
+        }
+
+        return Console.OpenStandardInput();
+    }
 }
-
-
-if (input == null)
-{
-    Console.Error.WriteLine("Failed to open stdin.");
-    return 1;
-}
-
-var segments = Framing.ReadSegments(input);
-var root = DeserializerState.CreateRoot(segments);
-var reader = CodeGeneratorRequest.READER.create(root);
-
-foreach (var node in reader.Nodes) {
-    Console.WriteLine($"Node: {node.DisplayName}");
-}
-
-return 0;
