3c54a510feca24ce6c5f3e4da5a4eaf14fcb1296
Izzy Lancaster <avrisaac555@gmail.com>
2024-07-28 17:05:34 -0700

    Made Compiler debugabble

--- Fundament.Capstone.Compiler/Program.cs
+++ Fundament.Capstone.Compiler/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System.Reflection;
-using System.Xml.Serialization;
 
 using Capnp;
+using Capnp.Schema;
 
-if (!Console.IsInputRedirected)
+var versionLine = $"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}";
+
+// Parse command-line arguments
+Stream input;
+
+if (args is [ "--version" ])
 {
-    Console.WriteLine($"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}");
-    Console.WriteLine("Expecting binary-ecoded code generation from stdin...");
+    Console.WriteLine(versionLine);
+    return 0;
+}
+else if (args is [ "--help"])
+{
+    Console.WriteLine(versionLine);
+    Console.WriteLine("Normally invoked by the Capstone compiler, this program reads a binary-encoded code generation request from a file or stdin if no file is provided.");
+    Console.WriteLine("Usage: capstone-csharp-compiler [path]");
+    Console.WriteLine("  --version  Print version information");
+    Console.WriteLine("  --help     Print this help");
+    Console.WriteLine("  [path]     Read binary-encoded code generation from file at [path]");
+    return 0;
+}
+else if (args is [ var pathArg ])
+{
+    input = File.OpenRead(pathArg);
+}
+else
+{
+    if (!Console.IsInputRedirected)
+    {
+        Console.WriteLine(versionLine);
+        Console.WriteLine("Expecting binary-ecoded code generation from stdin...");
+    }
+
+    input = Console.OpenStandardInput();
 }
 
-var input = Console.OpenStandardInput();
+
 if (input == null)
 {
     Console.Error.WriteLine("Failed to open stdin.");
@@ -18,6 +47,10 @@ if (input == null)
 
 var segments = Framing.ReadSegments(input);
 var root = DeserializerState.CreateRoot(segments);
-var reader = Capnp.Schema.CodeGeneratorRequest.READER.create(root);
+var reader = CodeGeneratorRequest.READER.create(root);
+
+foreach (var node in reader.Nodes) {
+    Console.WriteLine($"Node: {node.DisplayName}");
+}
 
 return 0;
