9d9e0d683f77515dd49074b21a519e10e6b5f2e3
Izzy Lancaster <avrisaac555@gmail.com>
2024-08-17 21:03:41 -0700

    Updated program to print Node tree

--- Fundament.Capstone.Compiler/Program.cs
+++ Fundament.Capstone.Compiler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Fundament.Capstone.Compiler;
+﻿global using Word = System.UInt64;
+
+namespace Fundament.Capstone.Compiler;
 
 using System.Reflection;
 
@@ -16,7 +18,8 @@ public static class Program
     public static int Main(string[] args)
     {
         var inputStream = ParseArgs(args);
-        if (inputStream is null) {
+        if (inputStream is null)
+        {
             return 0;
         }
 
@@ -24,8 +27,29 @@ public static class Program
         var root = DeserializerState.CreateRoot(segments);
         var reader = CodeGeneratorRequest.READER.create(root);
 
-        foreach (var node in reader.Nodes) {
-            Console.WriteLine($"Node: {node.DisplayName}");
+        foreach (var node in reader.Nodes) 
+        {
+            Console.WriteLine($"Node: {node.which} {node.DisplayName} Id: {node.Id} ParentId: {node.ScopeId}");
+            foreach (var annotation in node.Annotations)
+            {
+                Console.WriteLine($"\tAnnotation: {annotation.Id} {annotation.Brand} {annotation.Value}");
+            }
+
+            foreach (var nested in node.NestedNodes)
+            {
+                Console.WriteLine($"\t{nested.Name} - {nested.Id}");
+            }
+
+            if (node.which == Node.WHICH.Struct) {
+                foreach (var field in node.Struct.Fields)
+                {
+                    Console.WriteLine($"\tField: {field.which} {field.Name}");
+                    foreach (var annotation in node.Annotations)
+                    {
+                        Console.WriteLine($"\t\tAnnotation: {annotation.Id} {annotation.Brand.Scopes} {annotation.Value.which}");
+                    }
+                }
+            }
         }
 
         return 0;
