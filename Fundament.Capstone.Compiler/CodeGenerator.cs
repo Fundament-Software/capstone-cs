87101bb5edb9f828bc99aeede171b6271576cdef
Izzy Lancaster <avrisaac555@gmail.com>
2024-08-17 21:03:49 -0700

    Added CodeGenerator

---
+++ Fundament.Capstone.Compiler/CodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Fundament.Capstone.Compiler;
+
+using System.Collections.Immutable;
+using System.Data.Common;
+using System.Text;
+
+using Capnp.Schema;
+
+using Microsoft.Extensions.Logging;
+
+public class CodeGenerator()
+{
+    public void Generate()
+    {
+    }
+}
