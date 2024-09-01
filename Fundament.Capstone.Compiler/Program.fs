984a1fb654c0dc7fef2df286008e73efb6730d5a
Izzy Lancaster <avrisaac555@gmail.com>
2024-09-01 16:54:27 -0700

    Add Argu and basic code

--- Fundament.Capstone.Compiler/Program.fs
+++ Fundament.Capstone.Compiler/Program.fs
@@ -1,2 +1,59 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Argu
+open System
+open System.Reflection
+open Capnp
+open Capnp.Schema
+
+type Arguments =
+    | [<First; Unique>] File of string
+    | Version
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Version -> "Display the version of the compiler."
+            | File _ -> "Read a binary-encoded code generation request from the file at the specified path."
+
+let inputStreamResult =
+    let VersionLine =
+        $"Capstone C# Compiler Plugin Version {Assembly.GetExecutingAssembly().GetName().Version}"
+
+    let HelpTextMessage =
+        $"{VersionLine}\n\
+        Normally invoked by the Capstone compiler, this program reads a binary-encoded code generation request from a file or stdin if no file is provided."
+
+    let parser = ArgumentParser.Create<Arguments>()
+
+    let parsedArguments = parser.ParseCommandLine(raiseOnUsage = false)
+
+    if parsedArguments.IsUsageRequested then
+        do printfn "%s" (parser.PrintUsage(HelpTextMessage))
+        Error 1
+    elif parsedArguments.Contains(Version) then
+        do printfn "%s" VersionLine
+        Error 1
+    else
+        let s =
+            match parsedArguments.TryGetResult(File) with
+            | None ->
+                if not Console.IsInputRedirected then
+                    do printfn $"{VersionLine}\nExpecting binary-encoded code generation request from stdin..."
+
+                Console.OpenStandardInput()
+            | Some(path) -> IO.File.OpenRead path
+
+        Ok(s)
+
+let inputStream =
+    match inputStreamResult with
+    | Ok(inputStream) -> inputStream
+    | Error(code) -> exit code
+
+let reader =
+    inputStream
+    |> Framing.ReadSegments
+    |> DeserializerState.CreateRoot
+    |> CodeGeneratorRequest.READER.create
+
+for node in reader.Nodes do
+    printfn $"Node: {node.which} {node.DisplayName} Id: {node.Id} ParentId: {node.ScopeId}"
